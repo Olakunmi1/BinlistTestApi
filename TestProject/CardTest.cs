5a98b847800700b7f928829073318d5bb8474282
Olakunmi <60871836+Olakunmi1@users.noreply.github.com>
2021-03-31 20:20:10 +0100

    unitTest fixx

--- TestProject/CardTest.cs
+++ TestProject/CardTest.cs
@@ -23,7 +23,7 @@ namespace TestProject
         const int count2 = 4;
         private static List<HitCountsDTO_GetAll> GetAllCardHits()
         {
-            List<HitCountsDTO_GetAll> customers = new List<HitCountsDTO_GetAll>
+            List<HitCountsDTO_GetAll> cardHits = new List<HitCountsDTO_GetAll>
             {
                 new HitCountsDTO_GetAll
                 {
@@ -38,7 +38,17 @@ namespace TestProject
                 },
             };
 
-            return customers;
+            return cardHits;
+        }
+
+        private static MyRootClass GetCarddetails()
+        {
+            MyRootClass cardDetails = new MyRootClass
+            {
+               
+            };
+
+            return cardDetails;
         }
 
         [Fact]
@@ -54,6 +64,26 @@ namespace TestProject
             //act  --- calling on the method to be tested 
 
             var result = controller.GetAllCardHits();
+
+            //assert  --- i.e we need to start testing d outcome
+            var viewresult = result.Should().BeOfType<OkObjectResult>();
+            var model = viewresult.Subject.Value.Should().BeAssignableTo<ApiGenericResponseDTO<HitCountsDTO_GetAll>>();
+            model.Subject.Response.Count().Should().Be(2);
+        }
+
+        [Fact]
+        public void GetCardDetails_3rdPartyApi()
+        {
+            // arrange ---intializing the classes needed, and setup up mock
+            var mockcardservice = new Mock<ICardService>();
+            ILogger<CardController> logger = new Logger<CardController>(new NullLoggerFactory()); //mock for ilogger 
+            mockcardservice.Setup(c => c.GetcardDetails(CardNum)).Returns(GetAllCardHits());
+
+            var controller = new CardController(logger, mockcardservice.Object);
+
+            //act  --- calling on the method to be tested 
+
+            var result = controller.GetCardDetails();
 
             //assert  --- i.e we need to start testing d outcome
             var viewresult = result.Should().BeOfType<OkObjectResult>();
